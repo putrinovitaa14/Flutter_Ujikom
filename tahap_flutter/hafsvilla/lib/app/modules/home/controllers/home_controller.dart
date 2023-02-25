fc554734ea8e4dfea081c98423958727621dcdbd
putrinovitaa14 <putrinovitasari01411@gmail.com>
2023-02-25 08:40:24 +0700

    Latihan Flutter

--- tahap_flutter/hafsvilla/lib/app/modules/home/controllers/home_controller.dart
+++ tahap_flutter/hafsvilla/lib/app/modules/home/controllers/home_controller.dart
@@ -5,14 +5,17 @@ import 'package:get/get.dart';
 import '../../login/views/login_view.dart';
 
 class HomeController extends GetxController {
-  late Timer _pindah;
+  
   //TODO: Implement HomeController
 
   @override
+  late Timer _pindah;
+  final authToken = GetStorage();
   void onInit() {
    _pindah = Timer.periodic(
       const Duration(seconds: 4),
-        (timer) => Get.off(
+        (timer) => authToken.read('token') == null
+        ? Get.off(
           () => const LoginView(),
             transition: Transition.leftToRight,
   ),
